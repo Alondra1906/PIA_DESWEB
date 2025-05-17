31d90974f436af183a457b37e99e5ce6fae16b22
Alondra1906 <alondra.torress@uanl.edu.mx>
2025-05-16 20:03:14 -0600

    Se agregó inicio de la página

--- donmueble/urls.py
+++ donmueble/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index, contacto
+from .views import index, contacto, productos, nosotros, clientes
 
 urlpatterns = [
     path('', index,name='index'),
     path('contacto/', contacto,name='contacto'),
+    path('productos/', productos,name='productos'),
+    path('nosotros/', nosotros, name='nosotros'),
+    path('clientes/', clientes, name='clientes'),
 ]
--- donmueble/views.py
+++ donmueble/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def contacto(request):
     return render(request, 'contacto.html')
+
+def productos(request):
+    return render(request, 'productos.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def clientes(request):
+    return render(request, 'clientes.html')
