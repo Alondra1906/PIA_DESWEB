ccc3d669fc7559414713067eac3a550c892d6160
Alondra1906 <alondra.torress@uanl.edu.mx>
2025-05-19 00:46:54 -0600

    se agregó admin

--- donmueble/admin.py
+++ donmueble/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Sucursal, Empleado, Cliente
 
 # Register your models here.
 admin.site.register(Producto)
+admin.site.register(Sucursal)
+admin.site.register(Empleado)
+admin.site.register(Cliente)
